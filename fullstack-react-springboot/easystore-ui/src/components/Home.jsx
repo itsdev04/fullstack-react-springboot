6b81f69be0701a1cb23a1d9a7703dce0ffab8029
itsdev04 <imkrdev@gmail.com>
2026-02-10 20:20:23 +0530

    added styled-components

--- fullstack-react-springboot/easystore-ui/src/components/Home.jsx
+++ fullstack-react-springboot/easystore-ui/src/components/Home.jsx
@@ -1,11 +1,13 @@
 import PageHeading from "./PageHeading";
 import ProductListings from "./ProductListings";
 import products from "../data/products.js";
+import EazyButton from "./EazyButton.js";
 
 export default function Home () {
     return (
         <div className="home-container">
       <PageHeading title="Explore Eazy Stickers!">
+      <EazyButton>Home</EazyButton>
         Add a touch of creativity to your space with our wide range of fun and
         unique stickers. Perfect for any occasion!
       </PageHeading>
--- fullstack-react-springboot/easystore-ui/src/components/footer/Footer.jsx
+++ fullstack-react-springboot/easystore-ui/src/components/footer/Footer.jsx
@@ -1,12 +1,31 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHeart } from "@fortawesome/free-solid-svg-icons";
+import styled from "styled-components";
+import EazyButton from "../EazyButton";
 
-export default function Footer () {
-    return (
-        <footer> Built with 
-        <FontAwesomeIcon icon={faHeart} aria-label="hidden" className="footer-icon"></FontAwesomeIcon>
+const H1 = styled.h1`
+color: #5b21b6;
+text-align: center;`;
+
+export default function Footer() {
+  return (
+    <>
+      <H1>Demo of Styled-Components from Footer.jsx</H1>
+      <EazyButton>Submit</EazyButton> 
+      <footer>
+        {" "}
+        Built with
+        <FontAwesomeIcon
+          icon={faHeart}
+          aria-label="hidden"
+          className="footer-icon"
+        ></FontAwesomeIcon>
         by
-        <a href="https://easybytes.com" target="_blank" rel="noreferrer"> eazybytes</a>
-        </footer>
-    );
+        <a href="https://easybytes.com" target="_blank" rel="noreferrer">
+          {" "}
+          eazybytes
+        </a>
+      </footer>
+    </>
+  );
 }
