dc9c25d48a2a2b46bbb9ced5d3eb7ec3a8b514f6
itsdev04 <imkrdev@gmail.com>
2026-04-26 23:08:17 +0530

    theme and responsive

--- fullstack-react-springboot/easystore-ui/src/components/Home.jsx
+++ fullstack-react-springboot/easystore-ui/src/components/Home.jsx
@@ -53,6 +53,34 @@ export default function Home() {
             aria-label="Close"
           ></button>
         </div>
+        <div>
+          <div className="card" style={{ width: "18rem" }}>
+            <img src="..." className="card-img-top" alt="..." />
+            <div className="card-body">
+              <h5 className="card-title">Card title</h5>
+              <p className="card-text">
+                Some quick example text to build on the card title and make up
+                the bulk of the card’s content.
+              </p>
+              <a href="#" className="btn btn-primary">
+                Go somewhere
+              </a>
+            </div>
+          </div>
+          <div className="card" style={{ width: "18rem" }}>
+            <img src="..." className="card-img-top" alt="..." />
+            <div className="card-body">
+              <h5 className="card-title">Card title</h5>
+              <p className="card-text">
+                Some quick example text to build on the card title and make up
+                the bulk of the card’s content.
+              </p>
+              <a href="#" className="btn btn-primary">
+                Go somewhere
+              </a>
+            </div>
+          </div>
+        </div>
       </div>
       <PageHeading title="Explore Eazy Stickers!">
         <EazyButton $primary>Home</EazyButton>
