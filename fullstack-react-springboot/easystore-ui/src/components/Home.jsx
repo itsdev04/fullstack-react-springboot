b63ac1256a38dadea7f5057df2d85ad6d97671bc
itsdev04 <imkrdev@gmail.com>
2026-04-26 23:56:06 +0530

    styled using tailwind css for all components

--- fullstack-react-springboot/easystore-ui/src/components/Home.jsx
+++ fullstack-react-springboot/easystore-ui/src/components/Home.jsx
@@ -1,89 +1,11 @@
+import products from "../data/products";
 import PageHeading from "./PageHeading";
 import ProductListings from "./ProductListings";
-import products from "../data/products.js";
-import EazyButton from "./EazyButton.js";
-import BootstrapButton from "./BootstrapButton.jsx";
 
 export default function Home() {
   return (
-    <div className="home-container">
-      <div className="container col-6">
-        <BootstrapButton text="Bootstrap Button" type="primary" />
-
-        <BootstrapButton text="Bootstrap Button" type="secondary" />
-
-        <BootstrapButton text="Bootstrap Button" type="success" />
-
-        <BootstrapButton text="Bootstrap Button" type="danger" />
-
-        <BootstrapButton text="Bootstrap Button" type="primary" />
-      </div>
-      <div className="d-grid gap-2 col-6 mx-auto">
-        <div className="alert alert-primary" role="alert">
-          A simple primary alert—check it out!
-        </div>
-        <div className="alert alert-secondary" role="alert">
-          A simple secondary alert—check it out!
-        </div>
-        <div className="alert alert-success" role="alert">
-          A simple success alert—check it out!
-        </div>
-        <div className="alert alert-danger" role="alert">
-          A simple danger alert—check it out!
-        </div>
-        <div className="alert alert-warning" role="alert">
-          A simple warning alert—check it out!
-        </div>
-        <div className="alert alert-info" role="alert">
-          A simple info alert—check it out!
-        </div>
-        <div className="alert alert-light" role="alert">
-          A simple light alert—check it out!
-        </div>
-        <div
-          className="alert alert-dark alert-dismissible fade show"
-          role="alert"
-        >
-          A simple dark alert—check it out!
-          <strong>Warning!!</strong>
-          <button
-            type="button"
-            className="btn-close"
-            data-bs-dismiss="alert"
-            aria-label="Close"
-          ></button>
-        </div>
-        <div>
-          <div className="card" style={{ width: "18rem" }}>
-            <img src="..." className="card-img-top" alt="..." />
-            <div className="card-body">
-              <h5 className="card-title">Card title</h5>
-              <p className="card-text">
-                Some quick example text to build on the card title and make up
-                the bulk of the card’s content.
-              </p>
-              <a href="#" className="btn btn-primary">
-                Go somewhere
-              </a>
-            </div>
-          </div>
-          <div className="card" style={{ width: "18rem" }}>
-            <img src="..." className="card-img-top" alt="..." />
-            <div className="card-body">
-              <h5 className="card-title">Card title</h5>
-              <p className="card-text">
-                Some quick example text to build on the card title and make up
-                the bulk of the card’s content.
-              </p>
-              <a href="#" className="btn btn-primary">
-                Go somewhere
-              </a>
-            </div>
-          </div>
-        </div>
-      </div>
+    <div className="max-w-[1152px] mx-auto px-6 py-8">
       <PageHeading title="Explore Eazy Stickers!">
-        <EazyButton $primary>Home</EazyButton>
         Add a touch of creativity to your space with our wide range of fun and
         unique stickers. Perfect for any occasion!
       </PageHeading>
--- fullstack-react-springboot/easystore-ui/src/components/PageTitle.jsx
+++ fullstack-react-springboot/easystore-ui/src/components/PageTitle.jsx
@@ -1,10 +1,9 @@
-import React from 'react'
+import React from "react";
 
-function PageTitle(props) {
+export default function PageTitle({ title }) {
   return (
-    <h1 className='page-title'>{props.title}</h1>
-    
-  )
+    <h1 className="text-3xl font-primary font-extrabold text-center text-primary mt-4 py-2">
+      {title}
+    </h1>
+  );
 }
-
-export default PageTitle
